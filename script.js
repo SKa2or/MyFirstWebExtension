966ecc40102ee18ae7d5c0bc714eb1b0a84f1a0c
SKa2or <1838119962@qq.com>
2018-11-11 20:43:25 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+alert("My name is SKa2or!");
+document.getElementById("user[login]").value="SKa2or";
+document.getElementById("user[email]").value="shuojiang94@gmail.com";
+document.getElementById("user[password]").value="114514";
